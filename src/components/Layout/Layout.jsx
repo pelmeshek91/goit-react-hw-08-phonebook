409b920afd23fe9c2ef2c94c71130d55e582649a
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-06-14 21:31:28 +0100

    delete warning

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { AppBar } from 'components/AppBar/AppBar';
-import Navigate from 'components/Navigate/Navigate';
+
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
